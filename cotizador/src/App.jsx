92448596c75eebcbd9052085a56d0493094428e4
Denis Federico Zelaya <48973732+DenisFZelaya@users.noreply.github.com>
2022-03-16 13:34:08 -0600

    Primer avance

---
+++ cotizador/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import AppSeguro from './components/AppSeguro'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <AppSeguro />
+  )
+}
+
+export default App
---
+++ cotizador/src/components/Formulario.jsx
@@ -0,0 +1,13 @@
+export const Formulario = () => {
+  return (
+    <>
+      <form>
+        <div className="my-5">
+          <label className="block mb-3 font-bold text-gray-400 uppercase">
+            Marca
+          </label>
+        </div>
+      </form>
+    </>
+  );
+};
